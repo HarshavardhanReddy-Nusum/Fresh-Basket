92d7823fafc348efde7767a8f3b1e35ce6aca357
HarshavardhanReddy-Nusum <harshavardhanreddynusum@gmail.com>
2025-08-27 20:05:59 +0530

    modified

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ await connectDb()
 await connectCloudinary()
 
 // Allow multiple origins
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173','https://fresh-basket-theta.vercel.app']
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
